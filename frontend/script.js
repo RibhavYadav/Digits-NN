a27ad4620f844db47591700308229f14b9288e8a
draby2466 <ribhavyadav2466@gmail.com>
2025-01-20 22:31:22 +0530

    Added a canvas along with a button to reset the canvas.

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector('.canvas');
+const resetCanvas = document.getElementById('reset');
+const boxes = document.getElementsByClassName('box');
 let gridSize = 16;
+let isDrawing = false;
 
 // Create canvas
 for (let i = 0; i < gridSize ** 2; i++) {
@@ -7,3 +10,30 @@ for (let i = 0; i < gridSize ** 2; i++) {
     div.classList.add('box')
     canvas.appendChild(div);
 }
+
+// Function to color the box
+function color(e) {
+    if (isDrawing && e.target.classList.contains('box')) {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+// Add event listeners to color the box
+canvas.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    isDrawing = true;
+    color(e)
+})
+canvas.addEventListener('mousemove', (e) => {
+    color(e);
+});
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+// Reset the canvas
+resetCanvas.addEventListener('click', (e) => {
+    Array.from(boxes).forEach((box) => {
+        box.style.backgroundColor = 'white';
+    })
+})
